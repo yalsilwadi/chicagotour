375f5083879aa5a7de05a2cf2b2ff242e4bca6cd
yalsilwadi <yasseralsilwadi@gmail.com>
2023-07-10 17:09:58 -0500

    header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>CHICAGOTOUR</h1>
+      <div className="Title-Subtitle">Chicago Places to Visit</div>
+    </div>
+  )
+}
+
+export default Title
